a20b0bfbd9e2c4a26cc246294d8c405320300a0b
טל <talnoamb@gmail.com>
2018-11-13 10:15:51 +0200

    Junit test polynom : added polynom init test.

--- Object Oriented - course/src/Tests/Junit_Testing_Polynom.java
+++ Object Oriented - course/src/Tests/Junit_Testing_Polynom.java
@@ -2,17 +2,40 @@ package Tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Iterator;
+
 import org.junit.jupiter.api.Test;
 
 import myMath.Monom;
 import myMath.Polynom;
-import myMath.Polynom_able;
 
 class Junit_Testing_Polynom {
 
 	@Test
+	public void polynomTest() {
+		Monom m0 = new Monom(6,1);
+		Monom m1 = new Monom(5.4,2);
+		Monom m2 = new Monom(3.6,4);
+		double arr[] = {6,5.4,3.6};
+		int arr2[] = {1,2,4};
+		
+		Polynom p1 = new Polynom();
+		p1.add(m0); p1.add(m1); p1.add(m2);
+		Iterator<Monom> it = p1.iteretor();
+		int i = 0;
+		while(it.hasNext()) {
+			assertEquals(arr[i],it.next().get_coefficient());
+			i++;
+		}
+		i = 0;
+		while(it.hasNext()) {
+			assertEquals(arr2[i],it.next().get_power());
+			i++;
+		}
+	}
+	@Test
 	public void StringTest() {
-		Polynom_able p1 = new Polynom("5x^1");
+		Polynom p1 = new Polynom("5x^1");
 		Monom m = new Monom(5,1);
 		Polynom p2 = new Polynom();
 		p2.add(m);
@@ -35,6 +58,10 @@ class Junit_Testing_Polynom {
 		Polynom p3 = new Polynom ("1*x^2+2*x^1+1");
 		p1.multiply(p2);
 		assertTrue(p1.equals(p3));
+	}
+	@Test
+	public void AddMonomTest() {
+		
 	}
 	
 }
