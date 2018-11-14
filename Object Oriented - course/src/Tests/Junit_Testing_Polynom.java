3f13e156c971055323a7a3b5db2bd6790cf79f74
Oranit <Oranit@Oranit-HP>
2018-11-14 17:50:01 +0200

    update 0.0.3
    adding isZeroTest();;
    adding derivativeTest()

--- Object Oriented - course/src/Tests/Junit_Testing_Polynom.java
+++ Object Oriented - course/src/Tests/Junit_Testing_Polynom.java
@@ -19,7 +19,7 @@ class Junit_Testing_Polynom {
 		Monom m2 = new Monom(3.6,4);
 		double arr[] = {6,5.4,3.6};
 		int arr2[] = {1,2,4};
-		
+
 		Polynom p1 = new Polynom();
 		p1.add(m0); p1.add(m1); p1.add(m2);
 		Iterator<Monom> it = p1.iteretor();
@@ -35,8 +35,8 @@ class Junit_Testing_Polynom {
 		Polynom pEqual = new Polynom("1*x^1");
 		assertTrue(p.equals(pEqual));
 	}
-	
-	
+
+
 	@Test
 	public void StringTest() {
 		Polynom p1 = new Polynom("5x^1");
@@ -93,7 +93,7 @@ class Junit_Testing_Polynom {
 		Polynom pEqual = new Polynom("3*x^4 + 2*x^3 + 1*x^2");		
 		p1.substract(p2);
 		assertTrue(p1.equals(pEqual));
-		
+
 	}
 	public void isZeroTest() {
 		Polynom p = new Polynom("0");
@@ -101,5 +101,10 @@ class Junit_Testing_Polynom {
 		Monom m = new Monom (0,0);
 		p.add(m);
 		assertTrue(p.isZero());
+	}
+	public void derivativeTest() {
+		Polynom p = new Polynom("8x^4 + 7x^3 + 5x^2 + 3x^1 + 5");
+		Polynom Pderiv= new Polynom("32x^3+21x^2+10x^1+3");
+		assertTrue(p.derivative().equals(Pderiv));
 	}
 }
